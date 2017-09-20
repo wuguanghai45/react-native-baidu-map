2a9cfdf77fe06ec0d375159b7989d4478d32cb94
walt <walt@agideo.com>
2017-09-20 16:34:13 +0800

    升级ios 百度地图版本

--- ios/RCTBaiduMap/RCTBaiduMapView.h
+++ ios/RCTBaiduMap/RCTBaiduMapView.h
@@ -12,9 +12,22 @@
 
 #import <React/RCTViewManager.h>
 #import <React/RCTConvert+CoreLocation.h>
-#import <BaiduMapAPI_Map/BMKMapView.h>
+
+//#import <BaiduMapAPI_Map/BMKMapView.h>
+//#import <BaiduMapAPI_Map/BMKPinAnnotationView.h>
+//#import <BaiduMapAPI_Map/BMKPointAnnotation.h>
+
+#import <BaiduMapAPI_Base/BMKBaseComponent.h>//引入base相关所有的头文件
 #import <BaiduMapAPI_Map/BMKPinAnnotationView.h>
 #import <BaiduMapAPI_Map/BMKPointAnnotation.h>
+#import <BaiduMapAPI_Map/BMKMapView.h>
+//#import <BaiduMapAPI_Map/BMKMapComponent.h>//引入地图功能所有的头文件
+//#import <BaiduMapAPI_Search/BMKSearchComponent.h>//引入检索功能所有的头文件
+//#import <BaiduMapAPI_Cloud/BMKCloudSearchComponent.h>//引入云检索功能所有的头文件
+//#import <BaiduMapAPI_Location/BMKLocationComponent.h>//引入定位功能所有的头文件
+//#import <BaiduMapAPI_Utils/BMKUtilsComponent.h>//引入计算工具所有的头文件
+//#import <BaiduMapAPI_Radar/BMKRadarComponent.h>//引入周边雷达功能所有的头文件
+
 #import <UIKit/UIKit.h>
 
 @interface RCTBaiduMapView : BMKMapView <BMKMapViewDelegate>
